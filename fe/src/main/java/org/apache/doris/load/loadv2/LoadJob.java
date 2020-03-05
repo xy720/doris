639ab5df0185c8ec2110d6418d42d102d9627ee4
wyb <wyb@mba.local>
2020-03-06 02:57:53 +0800

    Submit PENDING task in spark load job

--- fe/src/main/java/org/apache/doris/load/loadv2/LoadJob.java
+++ fe/src/main/java/org/apache/doris/load/loadv2/LoadJob.java
@@ -29,6 +29,7 @@ import org.apache.doris.common.ErrorCode;
 import org.apache.doris.common.ErrorReport;
 import org.apache.doris.common.FeMetaVersion;
 import org.apache.doris.common.LabelAlreadyUsedException;
+import org.apache.doris.common.LoadException;
 import org.apache.doris.common.MetaNotFoundException;
 import org.apache.doris.common.UserException;
 import org.apache.doris.common.io.Text;
@@ -368,7 +369,8 @@ public abstract class LoadJob extends AbstractTxnStateChangeCallback implements 
      * @throws AnalysisException there are error params in job
      * @throws DuplicatedRequestException 
      */
-    public void execute() throws LabelAlreadyUsedException, BeginTransactionException, AnalysisException, DuplicatedRequestException {
+    public void execute() throws LabelAlreadyUsedException, BeginTransactionException,
+            AnalysisException, DuplicatedRequestException, LoadException {
         writeLock();
         try {
             unprotectedExecute();
@@ -377,8 +379,8 @@ public abstract class LoadJob extends AbstractTxnStateChangeCallback implements 
         }
     }
 
-    public void unprotectedExecute()
-            throws LabelAlreadyUsedException, BeginTransactionException, AnalysisException, DuplicatedRequestException {
+    public void unprotectedExecute() throws LabelAlreadyUsedException, AnalysisException,
+            BeginTransactionException, DuplicatedRequestException, LoadException {
         // check if job state is pending
         if (state != JobState.PENDING) {
             return;
@@ -386,7 +388,12 @@ public abstract class LoadJob extends AbstractTxnStateChangeCallback implements 
         // the limit of job will be restrict when begin txn
         beginTxn();
         unprotectedExecuteJob();
-        unprotectedUpdateState(JobState.LOADING);
+
+        if (jobType == EtlJobType.SPARK) {
+            unprotectedUpdateState(JobState.ETL);
+        } else {
+            unprotectedUpdateState(JobState.LOADING);
+        }
     }
 
     public void processTimeout() {
@@ -409,7 +416,7 @@ public abstract class LoadJob extends AbstractTxnStateChangeCallback implements 
         }
     }
 
-    protected void unprotectedExecuteJob() {
+    protected void unprotectedExecuteJob() throws LoadException {
     }
 
     /**
@@ -432,6 +439,9 @@ public abstract class LoadJob extends AbstractTxnStateChangeCallback implements 
         switch (jobState) {
             case UNKNOWN:
                 executeUnknown();
+                break;
+            case ETL:
+                //executeETL();
                 break;
             case LOADING:
                 executeLoad();
